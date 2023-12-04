db56dd895871cc789a6d3e6d38577c395f4133df
alesierra <alesierraalta@gmail.com>
2023-12-04 18:59:32 -0400

    version portafolio v0.0001

---
+++ miApp/tests.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+from django.urls import reverse
+
+class IndexPageTest(TestCase):
+    def test_index_page_loads(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
